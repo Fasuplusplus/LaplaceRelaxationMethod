5443b29e58e6d50d6675203ae072e0928bf41d64
Fasuplusplus <facundo_forte@hotmail.com>
2024-04-16 20:09:04 -0300

    hola

---
+++ LaplaceTwoSpheres.cpp
@@ -0,0 +1,8 @@
+// Proyecto más grande que el resto. Calcular el potencial electrostático alrededor de dos esferas conductoras de cargas conocidas usando método de relajación
+// Voy a plantear el problema como uno bidimensional en coordenadas cilindricas
+// Hola profe
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <cmath>
+using namespace std;
